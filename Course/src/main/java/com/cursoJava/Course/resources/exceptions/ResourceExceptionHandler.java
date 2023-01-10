e6f38a9e561da9616a293bdef087ef8c77160f85
davidrusyki <davidrusyck@gmail.com>
2023-01-10 17:29:59 -0300

    tratamento de exceção ao deletar

--- Course/src/main/java/com/cursoJava/Course/resources/exceptions/ResourceExceptionHandler.java
+++ Course/src/main/java/com/cursoJava/Course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.cursoJava.Course.services.exceptions.DatabaseException;
 import com.cursoJava.Course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,6 +21,17 @@ public class ResourceExceptionHandler {
 		String error = "Resource not found";
 		
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError erro = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(erro);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> dataBase(DatabaseException e,  HttpServletRequest request) {
+		
+		String error = "Data base error";
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError erro = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(status).body(erro);
